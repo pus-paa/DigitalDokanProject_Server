1377d03e3d0b9e80ea5a4f3732a4cfb6b0d30500
Puspa <puspa@example.com>
2025-06-09 01:17:32 +0545

    table creation

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { envConfig } from "./src/config/config";
 
 
 function startServer(){
-    const port = envConfig.port || 4000
+    const port = envConfig.port || 3003
 app.listen(envConfig.port,()=>{
     console.log(`server has started at port[${port}]`)
 })
